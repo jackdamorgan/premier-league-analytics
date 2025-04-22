6cf7c167029707681436ca6146e51238a1805ad5
Jack Morgan <morgandajack@gmail.com>
2025-04-22 10:54:48 +0300

    adding the raw material

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import requests
 import pandas as pd
 import psycopg2
+import json
 
+API_TOKEN = '40de67c3cbfe447493e2a4fcf1a54724'
+headers = { 'X-Auth-Token': API_TOKEN }
+url = 'https://api.football-data.org/v4/competitions/PL/matches?season=2023'
+
+response = requests.get(url, headers=headers)
+data = response.json()
+
+print(data.keys())
+
+with open("raw_matches.json", "w") as f:
+    json.dump(data, f, indent=4)
